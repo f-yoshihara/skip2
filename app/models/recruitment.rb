b32b07532f8faff4c88b7bbfdeea9f3035622d7e
吉原　史朗 <>
2018-08-13 01:38:26 +0900

    recruitmentモデル作成

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,7 +1,15 @@
 class Recruitment < ApplicationRecord
   belongs_to :company
   belongs_to :occupation
+  has_many :entry_sheets
+  has_many :staffs, through: :company
   enum status: {draft:0, published:1, deleted:2}
   validates :status,
+    presence: true
+  validates :title,
+    presence: true
+  validates :body,
+    presence: true
+  validates :deadline,
     presence: true
 end
