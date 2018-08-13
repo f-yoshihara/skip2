60c2a262862b99a63d1e0596e880001798381676
吉原　史朗 <>
2018-08-13 21:15:01 +0900

    staffモデル, studentモデル編集

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,4 +1,17 @@
 class Staff < ApplicationRecord
   belongs_to :company
   has_many :recruitments, through: :company
+  has_many :entry_sheets, through: :company
+  validates :family_name,
+    presence: true
+  validates :first_name,
+    presence: true
+  validates :password_digest,
+    presence: true
+  validates :email,
+    presence: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :phone,
+    presence: true,
+    format: { with: /\A\d{9,11}\z/ }
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,26 @@
 class Student < ApplicationRecord
+  has_many :interests
+  has_many :entry_sheets
+  validates :family_name,
+    presence: true
+  validates :first_name,
+    presence: true
+  validates :password_digest,
+    presence: true
+  validates :birthday,
+    presence: true
+  validates :email,
+    presence: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :phone,
+    presence: true,
+    format: { with: /\A\d{9,11}\z/ }
+  validates :dm,
+    presence: true
+  validates :school_year,
+    presence: true
+  validates :school_name,
+    presence: true
+  validates :school_course,
+    presence: true
 end
