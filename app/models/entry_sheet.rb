950e93da0c55aed8fda3dd1f710a5124cd8303c3
吉原　史朗 <>
2018-08-15 03:07:38 +0900

    フォーム日本語標記に変更

--- app/models/entry_sheet.rb
+++ app/models/entry_sheet.rb
@@ -4,16 +4,16 @@ class EntrySheet < ApplicationRecord
   belongs_to :company
   belongs_to :occupation
   has_many :staffs, through: :company
-  enum status: {draft:0, published:1, deleted:2}
+  enum status: { draft:0, published:1, deleted:2 }
   validates :status,
     presence: true
   validates :why,
     presence: true,
     length: {maximum: 400}
   validates :what,
-    presence: true
+    presence: true,
     length: {maximum: 400}
   validates :how,
-    presence: true
+    presence: true,
     length: {maximum: 400}
 end
