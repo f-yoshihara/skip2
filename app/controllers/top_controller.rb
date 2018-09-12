bcac2358b7ce323f1b63f3dc0810352ca076b4b5
吉原　史朗 <>
2018-09-12 19:33:03 +0900

    top画面変更

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,22 @@
 class TopController < ApplicationController
   def index
+    @recruitments = Recruitment.all
+    @url = url_for(controller: :get_photo, :id => @recruitment)
+    # ransack
+    @q = Recruitment.ransack(params[:q])
+    # @recruitment = @q.result(distinct: true)
+    # ransackのロジック
+    @result = @q.result
+    # view表示のロジック
+    result_ary = @result.to_a
+    @result_public_ary = []
+    result_ary.each do |result|
+      if result.status == 'published'
+        @result_public_ary << result
+      end
+    end
   end
+
   def about
   end
 end
