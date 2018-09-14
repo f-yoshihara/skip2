5ddfc7683fad4293a8001e594d1a2b1caa97e97b
吉原　史朗 <>
2018-09-14 12:59:20 +0900

    不要なコントローラ削除

--- app/controllers/line_controller.rb
+++
@@ -1,4 +0,0 @@
-class LineController < ApplicationController
-  def index
-  end
-end
--- test/controllers/line_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LineControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get line_index_url
-    assert_response :success
-  end
-
-end
