cfb1e4aa7058aa58c10d37f7b21fe61f70115ad5
吉原　史朗 <>
2018-09-16 17:40:10 +0900

    user がマイページから情報を編集できるようにしたいができない。deviseのフィルターが混入している？

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def index
   end
 
   def show
-    @user = current_user
     @stocks = @user.stocks
   end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'user was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:id, :name, :birth, :school_year, :school_name)
+    end
 end
