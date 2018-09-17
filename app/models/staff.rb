5ae11a1c03f2fde95148201b014f1cef399f5c2b
吉原　史朗 <>
2018-09-17 14:38:57 +0900

    企業担当者のemailを一意に設定

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -11,6 +11,7 @@ class Staff < ApplicationRecord
     presence: true
   validates :email,
     presence: true,
+    uniqueness: true,
     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :phone,
     presence: true,
