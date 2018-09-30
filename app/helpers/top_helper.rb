538205fe60aa67bfc61d8813c87ad17e4d23f1d4
吉原　史朗 <>
2018-09-30 18:10:08 +0900

    ボタンの表記とlink修正

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,14 +1,26 @@
 module TopHelper
-  def btn_text(result, text)
+  def btn_text(result, method, text)
     # if current_user.stocks.where(recruitment_id: result.id).exists?
     #   text + '済み'
     # else
     #   text + 'する'
     # end
-    if current_user && current_user.stocks.where(recruitment_id: result.id).exists?
+    if current_user && current_user.send(method).where(recruitment_id: result.id).exists?
       text + '済み'
     else
       text + 'する'
+    end
+  end
+
+  def btn_url(result, method)
+    if current_user && current_user.send(method).where(recruitment_id: result).exists?
+      # url_for系は使えない,path ならいける。
+      # stocks_destroy_path(result)
+      { :controller => method, :action => 'destroy', :format => result }
+    elsif method == 'entries'
+      { :controller => method, :action => 'new', :format => result }
+    else
+      { :controller => method, :action => 'create', :format => result }
     end
   end
 end
