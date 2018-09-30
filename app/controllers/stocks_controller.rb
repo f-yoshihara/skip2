8dfe59d8aa7c90b688669323b7d634cdcaa3319b
吉原　史朗 <>
2018-09-30 13:52:55 +0900

    destroy.js.erbに渡す変数を設定

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,10 +4,10 @@ class StocksController < InheritedResources::Base
     @recruitment_id = params[:format]
     @stock = Stock.new(user_id: @user_id, recruitment_id: @recruitment_id)
     @stock.save
-
   end
 
   def destroy
+    @recruitment_id = params[:format]
     @stock = Stock.find_by(recruitment_id: params[:format])
     @stock.destroy
   end
