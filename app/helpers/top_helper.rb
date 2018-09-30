b94211ae4eff7406645b8c320e83198df70ef115
吉原　史朗 <>
2018-09-30 15:12:01 +0900

    link_to_ifは条件式の真偽にかかわらずtextやurlを評価する。そのためログインしていないときにエラーが出ていたのを修正した。

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,6 +1,11 @@
 module TopHelper
   def btn_text(result, text)
-    if current_user.stocks.where(recruitment_id: result.id).exists?
+    # if current_user.stocks.where(recruitment_id: result.id).exists?
+    #   text + '済み'
+    # else
+    #   text + 'する'
+    # end
+    if current_user && current_user.stocks.where(recruitment_id: result.id).exists?
       text + '済み'
     else
       text + 'する'
