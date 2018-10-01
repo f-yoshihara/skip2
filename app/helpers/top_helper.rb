eac18dff3c2dc06a17e0225b80757c3e55fb6475
吉原　史朗 <>
2018-10-01 16:57:10 +0900

    viewのボタン部分がわかりにくかったので修正

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -14,7 +14,7 @@ module TopHelper
 
   def btn_url(result, method)
     if current_user && current_user.send(method).where(recruitment_id: result).exists?
-      # url_for系は使えない,path ならいける。
+      # url_for系とpathヘルパーは使えない。
       # stocks_destroy_path(result)
       { :controller => method, :action => 'destroy', :format => result }
     elsif method == 'entries'
@@ -22,5 +22,25 @@ module TopHelper
     else
       { :controller => method, :action => 'create', :format => result }
     end
+  end
+end
+
+def stock_url(result)
+  if current_user && current_user.stocks.where(recruitment_id: result).exists?
+    # stocks_destroy_path(result)
+    { :controller => 'stocks', :action => 'destroy', :format => result }
+  else
+    # stocks_create_path(result)
+    { :controller => 'stocks', :action => 'create', :format => result }
+  end
+end
+
+def entry_url(result)
+  if current_user && current_user.entries.where(recruitment_id: result).exists?
+    # edit_entry_path(result)
+    { :controller => 'entries', :action => 'destroy', :format => result }
+  else
+    # new_entry_path(result)
+    { :controller => 'entries', :action => 'new', :format => result }
   end
 end
