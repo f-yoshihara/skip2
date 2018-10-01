5e8555f3ebc10c38b6eb953de21bdb75790065a5
吉原　史朗 <>
2018-10-01 22:41:13 +0900

    ページネーション設置

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,23 +1,26 @@
 class TopController < ApplicationController
   def index
+    num_of_cards = 4
     @recruitments = Recruitment.all
     @occupations = Occupation.all
     @published_ary = make_published_ary(@recruitments)
     @url = url_for(controller: :get_photo, :id => @recruitment)
     # ransack
-    @q = Recruitment.ransack(params[:q])
+    @q = Recruitment.where(status: :published).ransack(params[:q])
     # @recruitment = @q.result(distinct: true)
     # ransackのロジック
-    @result = @q.result
+    @results = @q.result.page(params[:page]).per(num_of_cards)
     # view表示のロジック
-    result_ary = @result.to_a
-    @result_public_ary = make_published_ary(result_ary)
+    # result_ary = @result.to_a
+    # @result_public_ary = make_published_ary(result_ary).page(params[:page]).per(num_of_cards)
+    # @result_public_ary = @result.page(params[:page]).per(num_of_cards)
   end
 
   def about
   end
 
   private
+
     def make_published_ary(ary)
       new_ary = []
       ary.each do |result|
