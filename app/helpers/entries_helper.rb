e63d420007fa5edb83b7d45aad9831df2a34a21c
吉原　史朗 <>
2018-10-05 21:22:27 +0900

    お気に入り一覧ページ・応募一覧画面を作成

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,9 @@
 module EntriesHelper
+  def show_status(recruitment)
+    if recruitment.status == 'published'
+      '送信済み'
+    elsif recruitment.status == 'draft'
+      '下書き' 
+    end
+  end
 end
