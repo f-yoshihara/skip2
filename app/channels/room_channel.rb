988131a01257e35cca2afe7f48974b4e2d62ce06
吉原　史朗 <>
2018-11-01 07:31:21 +0900

    render 修正

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,12 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
     stream_from "room_channel"
+    # room = Room.find(params['room'])
+    # if current_user.can_access?(room)
+    #   stream_from "messages_#{params['room']}_channel"
+    # else
+    #   reject
+    # end
   end
 
   def unsubscribed
@@ -10,6 +16,14 @@ class RoomChannel < ApplicationCable::Channel
   def speak(data)
     message = Message.new(content: data['message'])
     message.save
-    ActionCable.server.broadcast 'room_channel', message: data['message']
+    ActionCable.server.broadcast 'room_channel', message: render_message(message)
   end
+
+  private 
+    def render_message(message)
+      ApplicationController.render(
+        partial: 'messages/message',
+        locals:  { message: message }
+      )
+    end
 end
