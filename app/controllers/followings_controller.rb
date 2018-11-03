78729f0d867e8d0d68b01002232d6fcfdf47cf2a
吉原　史朗 <>
2018-11-03 15:44:03 +0900

    follow機能リファクタリング

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,18 +1,29 @@
 class FollowingsController < ApplicationController
+  # before_action :set_following, only: [:index, :destroy]
   def index
     teacher_id = params[:id]
-    followings = Following.where(teacher_id: @teacher_id)
+    @followings = Following.where(teacher_id: @teacher_id)
   end
 
   def create
     @company_id = params[:company_id]
-    following = Following.new(teacher_id: session[:teacher], company_id: params[:company_id])
-    following.save
+    @following = Following.new(teacher_id: session[:teacher], company_id: params[:company_id])
+    @following.save
   end
 
   def destroy
-    @company_id = params[:id]
-    following = Following.find_by(company_id: params[:id])
+    @company_id = params[:company_id]
+    following = Following.find(params[:id])
     following.destroy
   end
+
+  private
+  # def set_following
+  #   set_teacher
+  #   @following = @teacher.followings.where(company_id: params[:id])
+  # end
+
+  # def set_teacher
+  #   @teacher = Teacher.find_by(session[:teacher].to_s)
+  # end
 end
