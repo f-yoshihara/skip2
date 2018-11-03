3da744dc387cde00842499a81acfa70b21a69ca1
吉原　史朗 <>
2018-11-03 22:48:59 +0900

    例外処理追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,25 @@ class ApplicationController < ActionController::Base
   private
     def current_staff
       Staff.find(session[:staff])
+    rescue ActiveRecord::RecordNotFound
+      false
     end
 
     def current_teacher
       Teacher.find(session[:teacher])
+    rescue ActiveRecord::RecordNotFound
+      false
     end
 
     def current_teachers_school
       School.find(current_teacher.school_id)
+    rescue NoMethodError
+      false
     end
 
     def current_staffs_company
       Company.find(current_staff.company_id)
+    rescue NoMethodError
+      false
     end
 end
