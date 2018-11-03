2ba90906a71d8071009dfce84843078b11f52af4
吉原　史朗 <>
2018-11-03 16:07:25 +0900

    followingコントローラ修正とindex表示のリクエスト変更

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,29 +1,22 @@
 class FollowingsController < ApplicationController
-  # before_action :set_following, only: [:index, :destroy]
+  before_action :set_company_id, only: [:create, :destroy]
+
   def index
-    teacher_id = params[:id]
-    @followings = Following.where(teacher_id: @teacher_id)
+    @followings = Following.where(teacher_id: current_teacher.id)
   end
 
   def create
-    @company_id = params[:company_id]
     @following = Following.new(teacher_id: session[:teacher], company_id: params[:company_id])
     @following.save
   end
 
   def destroy
-    @company_id = params[:company_id]
     following = Following.find(params[:id])
     following.destroy
   end
 
   private
-  # def set_following
-  #   set_teacher
-  #   @following = @teacher.followings.where(company_id: params[:id])
-  # end
-
-  # def set_teacher
-  #   @teacher = Teacher.find_by(session[:teacher].to_s)
-  # end
+  def set_company_id
+    @company_id = params[:company_id]
+  end
 end
