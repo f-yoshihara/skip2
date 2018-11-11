bbbcc6441b6da0b85aa1245c9d71046a341f94cd
吉原　史朗 <>
2018-11-11 16:23:48 +0900

    不要な部分削除

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,14 +1,2 @@
 module RoomsHelper
-  # def html_class?(message, session)
-  #   if message.school && session[:teacher]
-  #     'text-right'
-  #     puts('ok')
-  #   elsif !message.school && session[:staff]
-  #     'text-right'
-  #   else
-  #     'text-left'
-  #   end
-  # end
-
-  # <p class='text-right'>message.content</p>
 end
