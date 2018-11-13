6bc410dd890b74af4ee99efd6eada5ead69e3349
吉原　史朗 <>
2018-11-14 00:05:55 +0900

    navbarの表記変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  private
+    def current_staff
+      Staff.find(session[:staff])
+    rescue ActiveRecord::RecordNotFound
+      false
+    end
+
+    def current_teacher
+      Teacher.find(session[:teacher])
+    rescue ActiveRecord::RecordNotFound
+      false
+    end
 end
