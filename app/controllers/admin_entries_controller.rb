f53dda48e241e4dcd08e441a54e4a14a03092411
吉原　史朗 <>
2018-11-23 21:44:30 +0900

    採用担当者用応募状況確認画面作成

--- app/controllers/admin_entries_controller.rb
+++ app/controllers/admin_entries_controller.rb
@@ -1,5 +1,7 @@
 class AdminEntriesController < ApplicationController
   def index
+    @recruitment = Recruitment.find_by(id: params[:id])
+    @entries = @recruitment.entries.where(status: :published)
   end
 
   def show
